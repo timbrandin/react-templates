37e3ec518dfa4f895a98982ada4a78341d2ebcbb
Tim Brandin <tim.brandin@gmail.com>
2015-10-31 19:38:56 +0100

    Added required description.

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'timbrandin:react-templates',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'React Templates for Meteor',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/timbrandin/react-templates',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
